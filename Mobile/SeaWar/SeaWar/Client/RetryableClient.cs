5ca80376a1f99bec0b48323647f7c1d9380ded04
RomanDubinin <romandubinin94@gmail.com>
2020-05-10 18:40:37 +0500

    RetryableClient

---
+++ Mobile/SeaWar/SeaWar/Client/RetryableClient.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using SeaWar.Client.Contracts;
+
+namespace SeaWar.Client
+{
+    public class RetryableClient : IClient
+    {
+        private readonly IClient client;
+        private readonly int retryCount;
+
+        public RetryableClient(IClient client, int retryCount)
+        {
+            this.client = client;
+            this.retryCount = retryCount;
+        }
+
+
+        public async Task<RoomResponse> CreateRoomAsync(CreateRoomParameters parameters)
+        {
+            return await RetryAsync(client.CreateRoomAsync, parameters).ConfigureAwait(false);
+        }
+
+        public async Task<RoomResponse> GetRoomStatusAsync(GetRoomStatusParameters parameters)
+        {
+            return await RetryAsync(client.GetRoomStatusAsync, parameters).ConfigureAwait(false);
+        }
+
+        public async Task<GameStatusResponse> GetGameStatusAsync(GetGameStatusParameters parameters)
+        {
+            return await RetryAsync(client.GetGameStatusAsync, parameters).ConfigureAwait(false);
+        }
+
+        public async Task<GameFireResponse> FireAsync(FireParameters parameters)
+        {
+            return await RetryAsync(client.FireAsync, parameters).ConfigureAwait(false);
+        }
+
+        public async Task<T2> RetryAsync<T1, T2>( Func<T1, Task<T2>> func, T1 parameters)
+        {
+            var retries = 0;
+            Exception exception = null;
+            while (retries < retryCount)
+            {
+                try
+                {
+                    return await func(parameters).ConfigureAwait(false);
+                }
+                catch (Exception e)
+                {
+                    exception = e;
+                }
+                retries++;
+            }
+            throw exception;
+        }
+    }
+}
