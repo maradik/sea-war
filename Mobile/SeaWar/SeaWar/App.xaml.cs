7f7a15f74ffab0defb8d3231684e25d3ef0e5541
Кащенко Павел Анатольевич <p.kashchenko@kontur>
2020-05-10 15:11:24 +0500

    Страница с вводом имени пользователя

--- Mobile/SeaWar/SeaWar/App.xaml.cs
+++ Mobile/SeaWar/SeaWar/App.xaml.cs
@@ -12,7 +12,7 @@ namespace SeaWar
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new WelcomePage());
         }
 
         protected override void OnStart()
---
+++ Mobile/SeaWar/SeaWar/ViewModel/WelcomePageModelView.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using SeaWar.Annotations;
+using Xamarin.Forms;
+
+namespace SeaWar.ViewModel
+{
+    public class WelcomePageModelView : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string userName;
+
+        public string UserName
+        {
+            get => userName;
+            set
+            {
+                userName = value;
+                OnPropertyChanged(nameof(UserName));
+            }
+        }
+
+        public Command StartGame { get; }
+
+        public WelcomePageModelView()
+        {
+            StartGame = new Command(async _ =>
+            {
+                var mainPage = new MainPage();
+                await Application.Current.MainPage.Navigation.PushAsync(mainPage);
+            });
+        }
+        
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
