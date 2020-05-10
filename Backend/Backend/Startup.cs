f3ad91ce4f11af8d21a21c75bc49b42960b6d4b9
sboev <sboev@kontur.ru>
2020-05-10 15:59:35 +0500

    DI

--- Backend/Backend/Startup.cs
+++ Backend/Backend/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend.Controllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace Backend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSingleton(new RoomManager());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
