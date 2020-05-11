34a9740b0c12d57ab50457dd20557623f39417da
Кащенко Павел Анатольевич <p.kashchenko@kontur>
2020-05-11 13:20:52 +0500

    Костыль но работает

--- Mobile/SeaWar/SeaWar/View/GamePage.xaml.cs
+++ Mobile/SeaWar/SeaWar/View/GamePage.xaml.cs
@@ -12,5 +12,33 @@ namespace SeaWar.View
             InitializeComponent();
             BindingContext = createViewModel(model);
         }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    var image = new Image()
+                    {
+                        Source = "empty_cell.jpg"
+                    };
+                    
+                    var tapGestureRecognizer = new TapGestureRecognizer();
+                    tapGestureRecognizer.Tapped += (s, e) => {
+                        // handle the tap
+
+                        var tapImage = (Image)s;
+                        tapImage.Source = ImageSource.FromFile("miss_cell.jpg");
+                        int a = 10;
+                    };
+                    image.GestureRecognizers.Add(tapGestureRecognizer);
+                    PlayerGrid.Children.Add(image,i,j);
+                }
+            }
+            
+        }
     }
 }
