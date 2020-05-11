5a366cf0e3752f3c182dc72692dc7ef81c42dbc0
chdmitr <chdmitr@skbkontur.ru>
2020-05-11 15:49:44 +0500

    update tests

--- Backend/test/TestRequests.ps1
+++ Backend/test/TestRequests.ps1
@@ -1,31 +1,15 @@
-﻿#Создание первого игрока
-$body = "{ PlayerName: 'name1' }"
-$head = @{'content-type' = 'application/json'}
-Write-Host "`nЗапрос на создание комнаты с игроком 'name1'" -ForegroundColor Green
-$result = Invoke-WebRequest -Uri "http://5.189.18.251:8765/room/create" -Method Post -Body $body -Headers $head -UseBasicParsing
-$roomGuid = $result.Content | ConvertFrom-Json | Select-Object -ExpandProperty roomId
-$player1ID = $result.Content | ConvertFrom-Json | Select-Object -ExpandProperty playerId
-$result.Content
+﻿$ErrorActionPreference = 'Stop'
 
-#Запрашиваем состояние комнаты 
-Write-Host "`nЗапрашиваем состояние комнаты $roomGuid" -ForegroundColor Green
-$result = Invoke-WebRequest -Uri "http://5.189.18.251:8765/room/$roomGuid/getStatus?playerId=$player1ID" -Method get -UseBasicParsing
-$result.Content | ConvertFrom-Json | fl *
-
-#Создание второго игрока
-$body = "{ PlayerName: 'name2' }"
-$head = @{'content-type' = 'application/json'}
-Write-Host "`nЗапрос на создание комнаты с игроком 'name2'`n" -ForegroundColor Green
-$result = Invoke-WebRequest -Uri "http://5.189.18.251:8765/room/create" -Method Post -Body $body -Headers $head -UseBasicParsing
-$player2ID = $result.Content | ConvertFrom-Json | Select-Object -ExpandProperty playerId
-$result.Content
-$roomGuid = $result.Content | ConvertFrom-Json | Select-Object -ExpandProperty roomId
-
-#Запрашиваем состояние комнаты 
-Write-Host "`nGET запрос с guid" -ForegroundColor Green
-$result = Invoke-WebRequest -Uri "http://5.189.18.251:8765/room/$roomGuid/getStatus?playerId=$player1ID" -Method get -UseBasicParsing
-$result.Content | ConvertFrom-Json | fl *
-
+function addPlayer($playername){
+    $body = "{ PlayerName: '$playername' }"
+    $head = @{'content-type' = 'application/json'}
+    Write-Host "`nЗапрос на создание комнаты с игроком '$playername'" -ForegroundColor Green
+    $result = Invoke-WebRequest -Uri "http://5.189.18.251:8765/room/create" -Method Post -Body $body -Headers $head -UseBasicParsing
+    $roomGuid = $result.Content | ConvertFrom-Json | Select-Object -ExpandProperty roomId
+    $player1ID = $result.Content | ConvertFrom-Json | Select-Object -ExpandProperty playerId
+    write-host $result.Content -ForegroundColor Cyan
+    return $roomGuid, $player1ID    
+}
 
 function Fire($x, $y){
     Write-Host "`nСтреляем в $x`:$y" -ForegroundColor Green
@@ -33,6 +17,12 @@ function Fire($x, $y){
     $head = @{'content-type' = 'application/json'}
     $result = Invoke-WebRequest -Uri "http://5.189.18.251:8765/room/$roomGuid/game/fire?playerId=$player1ID" -Method POST -Body $body -Headers $head -UseBasicParsing
     return ($result.Content | ConvertFrom-Json | select -ExpandProperty enemymap).cells
+}
+
+function RoomStatus($roomGuid, $player1ID){
+    Write-Host "`nЗапрашиваем состояние комнаты $roomGuid" -ForegroundColor Green
+    $result = Invoke-WebRequest -Uri "http://5.189.18.251:8765/room/$roomGuid/getStatus?playerId=$player1ID" -Method get -UseBasicParsing
+    write-host $result.Content -ForegroundColor Cyan
 }
 
 function WriteMap($cells){
@@ -52,7 +42,20 @@ function WriteMap($cells){
     }
 }
 
+#Создание первого игрока
+$roomGuid, $player1ID = addPlayer 'name1'
+
+#Запрашиваем состояние комнаты
+RoomStatus $roomGuid $player1ID
+
+#Создание второго игрока
+$roomGuid, $player2ID = addPlayer 'name2'
+
+#Запрашиваем состояние комнаты
+RoomStatus $roomGuid $player1ID
+
 #fire
 WriteMap (Fire 0 0)
 WriteMap (Fire 1 1)
 WriteMap (Fire 9 0)
+WriteMap (Fire 5 0)
