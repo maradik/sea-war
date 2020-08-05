aa676abe39216fb45e255e78f704478db380d0ab
Anton Yuzhanin <a.yuzhanin@skbkontur.ru>
2020-08-05 15:25:35 +0500

    Поменял IP бэкенда

--- Mobile/SeaWar/SeaWar/Settings.cs
+++ Mobile/SeaWar/SeaWar/Settings.cs
@@ -4,7 +4,7 @@ namespace SeaWar
 {
     public static class Settings
     {
-        public static readonly string ServerUri = "http://5.189.18.251:8765/";
+        public static readonly string ServerUri = "http://91.224.22.81:8765/";
         public static readonly TimeSpan Timeout = TimeSpan.FromSeconds(5);
     }
 }
